293afd88f429c2b6a6fa68db8bbf0a24a2f0ca15
Marcos Pividori <marcos.pividori@gmail.com>
2013-06-23 16:46:14 -0300

    Changes in module hierarchy, add .cabal file and GCMAppConfig data type.

---
+++ push-notifications/gcm/test/main.hs
@@ -0,0 +1,29 @@
+
+import Gcm.Types
+import Gcm.Send
+import System.Environment
+import Data.Aeson
+import Data.Text    (Text, pack)
+import Data.Default
+import Data.HashMap.Strict
+
+myGcmApp :: GCMAppConfig
+myGcmApp = GCMAppConfig
+    {   apiKey = "key=" -- API Key. (provided by Google)
+    ,   projectId = "" -- Project ID. (provided by Google)
+    }
+
+-- Simple example, main regId message, sends message to the regId.
+main :: IO ()
+main = do
+            regId : msg : _ <-  getArgs
+            let
+                message = def {
+                                registration_ids = Just [regId]
+                            ,   data_object = Just (fromList [(pack "Message" .= (msg :: String))])
+                            ,   delay_while_idle = True
+                            ,   time_to_live = Just 120
+                            ,   dry_run = True
+                            }
+            result          <-  sendGCM myGcmApp message 1
+            print $ show result
