ad77310c9e9ade423b368d2167b5171c44a69519
Marcos Pividori <marcos.pividori@gmail.com>
2013-06-22 22:53:49 -0300

    Changes in main structure

--- PushNotifications/GCM/ApiKey.hs
+++
@@ -1,6 +0,0 @@
--- GSoC 2013 - Communicating with mobile devices.
-
--- | This Module define the ApiKey for sending Push Notifications through Google Cloud Messaging.
-module ApiKey where
-    apiKey :: String
-    apiKey = "" --API Key. (provided by Google)
