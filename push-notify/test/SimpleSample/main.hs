39bbdbd459d3cdf17b47237492ad8ab7cd1989a9
Marcos Pividori <marcos.pividori@gmail.com>
2013-06-28 14:10:05 -0300

    Just details

--- push-notify/test/SimpleSample/main.hs
+++ push-notify/test/SimpleSample/main.hs
@@ -11,8 +11,8 @@ import Data.HashMap.Strict
 
 myGcmApp :: GCMAppConfig
 myGcmApp = GCMAppConfig
-    {   apiKey = "key=" -- API Key. (provided by Google)
-    ,   projectId = "" -- Project ID. (provided by Google)
+    {   apiKey = pack "key=" -- API Key. (provided by Google)
+    ,   projectId = pack "" -- Project ID. (provided by Google)
     }
 
 -- Simple example, main regId message, sends message to the regId.
@@ -21,11 +21,11 @@ main = do
             regId : msg : _ <-  getArgs
             let
                 message = def {
-                                registration_ids = Just [regId]
+                                registration_ids = Just [pack regId]
                             ,   data_object = Just (fromList [(pack "Message" .= (msg :: String))])
-                            ,   delay_while_idle = True
-                            ,   time_to_live = Just 120
-                            ,   dry_run = True
+                            --,   delay_while_idle = True
+                            --,   time_to_live = Just 120
+                            --,   dry_run = True
                             }
             result          <-  sendGCM myGcmApp message 0
             print $ show result
