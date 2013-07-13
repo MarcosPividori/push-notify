e6cbfdd562d7e3818bcfb791f5d760e2ddc2e132
Marcos Pividori <marcos.pividori@gmail.com>
2013-07-13 18:05:47 -0300

    details in test example

--- push-notify/test/testApn/My-server/example.hs
+++ push-notify/test/testApn/My-server/example.hs
@@ -6,50 +6,38 @@ import Data.Default
 import Data.Text (pack)
 import Control.Concurrent
 
-{-
-main :: IO ()
-main = do
-            let confg = def{
-                            certificate = "public-cert.pem"
-                        ,   privateKey = "private-key.pem"
-                        ,   environment = Development }
-            putStrLn "LET'S SEND A NOTIFICATION:"
-            putStr "A device token (hexadecimal): "
-            dtoken      <- getLine
-            putStr "An alert message: "
-            alertMsg    <- getLine
-            let msg = def { deviceTokens = [pack dtoken,pack "3242"], alert = Left $ pack alertMsg }
-            newConfg    <- startAPNS confg
-            res         <- sendAPNS newConfg msg
-            putStrLn ("Result: " ++ show res)
-            putStrLn "\nLET'S CONNECT WITH FEEDBACK SERVICE:"
-            fres        <- feedBackAPNS confg
-            putStrLn ("Result: " ++ show fres)
--}
-
 main :: IO ()
 main = do
             let confg = def{
                             certificate = "public-cert.pem"
                         ,   privateKey  = "private-key.pem"
                         ,   environment = Development 
-                        ,   timeoutTime = 100000 }
+                        ,   timeoutTime = 1000000 
+                        }
 
-            let msg = def{
-                            deviceTokens = map (pack . show) [1 .. 100]
-                        ,   alert        = Left $ pack "Hello World!" }
             newConfg    <- startAPNS confg
             var         <- newEmptyMVar
             putMVar var ()
-            loop 5 msg newConfg var
+            loop 10000 newConfg var
             where
-                loop 0 msg newConfg var = do
-                                            threadDelay 200000
+                loop 0 newConfg var = do
+                                            threadDelay 50000000
                                             closeAPNS newConfg
-                loop n msg newConfg var = do            
+                loop n newConfg var = do
                            forkIO $ do
-                                        res <- sendAPNS newConfg msg
-                                        takeMVar var
-                                        putStrLn ("Result: " ++ show res)
-                                        putMVar var ()
-                           loop (n-1) msg newConfg var
+                                        n <- myThreadId
+                                        let msg = def{
+                                                deviceTokens = map (pack . show) [11 .. 20]
+                                            ,   alert        = Left $ pack ("Hello World! by: " ++ show n) }
+                                        sending msg newConfg var
+                           loop (n-1) newConfg var
+                
+                sending msg newConfg var = do
+                                                res <- sendAPNS newConfg msg
+                                                takeMVar var
+                                                putStrLn ("Result: " ++ show res)
+                                                putMVar var ()
+                                                let rest = toReSendTokens res
+                                                if rest /= []
+                                                then sending msg{deviceTokens = rest} newConfg var
+                                                else return ()
