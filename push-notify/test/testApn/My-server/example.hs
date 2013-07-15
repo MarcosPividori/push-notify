3d6b8e9dcb02c027940c747bac95b9df5a03ee8c
Marcos Pividori <marcos.pividori@gmail.com>
2013-07-14 23:57:30 -0300

    details

--- push-notify/test/testApn/My-server/example.hs
+++ push-notify/test/testApn/My-server/example.hs
@@ -6,38 +6,62 @@ import Data.Default
 import Data.Text (pack)
 import Control.Concurrent
 
-main :: IO ()
-main = do
+example1 :: IO ()
+example1 = do
+            let confg = def{
+                            certificate = "public-cert.pem"
+                        ,   privateKey = "private-key.pem"
+                        ,   environment = Development }
+            
+            putStrLn "LET'S SEND A NOTIFICATION:"
+            putStr "A device token (hexadecimal): "
+            
+            dtoken      <- getLine
+            
+            putStr "An alert message: "
+            
+            alertMsg    <- getLine
+            
+            let msg = def { deviceTokens = [pack dtoken], alert = Left $ pack alertMsg }
+            manager     <- startAPNS confg
+            res         <- sendAPNS manager msg
+            putStrLn ("Result: " ++ show res)
+            closeAPNS manager
+            putStrLn "\nLET'S CONNECT WITH FEEDBACK SERVICE:"
+            fres        <- feedBackAPNS confg
+            putStrLn ("Result: " ++ show fres)
+
+example2 :: IO ()
+example2 = do
             let confg = def{
                             certificate = "public-cert.pem"
                         ,   privateKey  = "private-key.pem"
-                        ,   environment = Development 
-                        ,   timeoutTime = 1000000 
+                        ,   environment = Development
                         }
 
-            newConfg    <- startAPNS confg
+            manager     <- startAPNS confg
             var         <- newEmptyMVar
             putMVar var ()
-            loop 10000 newConfg var
+            loop 10000 manager var
             where
-                loop 0 newConfg var = do
+                loop 0 manager var = do
                                             threadDelay 50000000
-                                            closeAPNS newConfg
-                loop n newConfg var = do
+                                            closeAPNS manager
+                loop n manager var = do
                            forkIO $ do
                                         n <- myThreadId
                                         let msg = def{
                                                 deviceTokens = map (pack . show) [11 .. 20]
                                             ,   alert        = Left $ pack ("Hello World! by: " ++ show n) }
-                                        sending msg newConfg var
-                           loop (n-1) newConfg var
+                                        sending msg manager var
+                           loop (n-1) manager var
                 
-                sending msg newConfg var = do
-                                                res <- sendAPNS newConfg msg
+                sending msg manager var = do
+                                                res <- sendAPNS manager msg
                                                 takeMVar var
                                                 putStrLn ("Result: " ++ show res)
                                                 putMVar var ()
                                                 let rest = toReSendTokens res
                                                 if rest /= []
-                                                then sending msg{deviceTokens = rest} newConfg var
-                                                else return ()
+                                                 then sending msg{deviceTokens = rest} manager var
+                                                 else return ()
