d2388f6b09344270cac56ebb7f6498b5ea5c5570
Marcos Pividori <marcos.pividori@gmail.com>
2013-07-27 22:06:27 -0300

    Many changes in the Yesod App. I add the PushNotify module to provide a common abstraction for the 3 push libraries

--- push-notify/test/Yesod-App/yesodApp.hs
+++ push-notify/test/Yesod-App/yesodApp.hs
@@ -1,6 +1,6 @@
--- Test Example for GCM Api.
+-- Test Example for Push Notifications.
 -- This is a simple example of a Yesod server, where devices can register to receive
--- GCM messages and users can send messages through the web service.
+-- messages and users can send messages through the web service.
 -- Before running this app, its necessary to complete the "approot" and the "apiKey" with the proper values.
 
 {-# LANGUAGE OverloadedStrings, TypeFamilies, TemplateHaskell,
@@ -22,7 +22,6 @@ import qualified Data.Map           as M
 import Control.Monad.Trans.Resource (runResourceT)
 import Control.Monad.IO.Class       (liftIO)
 import Control.Monad.Logger
---import Network (withSocketsDo)
 import Network.PushNotify.Gcm.Types
 import Network.PushNotify.Gcm.Send
 import Network.PushNotify.Mpns.Types
@@ -31,7 +30,7 @@ import Network.HTTP.Conduit
 import qualified Control.Exception  as CE
 import Data.Aeson.Types
 import Data.Monoid                  ((<>))
-import General
+import PushNotify
 
 -- Data Base:
 
@@ -39,9 +38,9 @@ share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
 Devices
     user Text
     password Text
-    identifier Ident
+    identifier Device
     UniqueUser user
-    UniqueIdent identifier
+    UniqueDevice identifier
     deriving Show
 |]
 
@@ -53,7 +52,7 @@ staticFiles "static"
 data Messages = Messages {
                             connectionPool :: ConnectionPool -- Connection to the Database.
                          ,  getStatic :: Static -- Reference point of the static data.
-                         ,  appConfig :: AppConfig -- configuration.
+                         ,  appConfig :: PushAppConfig -- configuration.
                          ,  manager   :: Manager
                          }
 
@@ -70,7 +69,7 @@ instance Yesod Messages where
     approot = ApprootStatic "" -- Here you must complete with the correct route.
 
 instance YesodPersist Messages where
-    type YesodPersistBackend Messages = SqlPersist
+    type YesodPersistBackend Messages = SqlPersistT
     runDB action = do
         Messages pool _ _ _ <- getYesod
         runSqlPool action pool
@@ -81,7 +80,7 @@ instance RenderMessage Messages FormMessage where
 -- Handlers:
 
 -- 'getRootR' provides the main page.
-getRootR :: Handler RepHtml
+getRootR :: Handler Html
 getRootR = do
                 his   <- lookupSession "history"
                 list  <- runDB $ selectList [] [Desc DevicesUser]
@@ -115,26 +114,27 @@ postRegisterR = do
     usr    <- runInputPost $ ireq textField "user"
     pass   <- runInputPost $ ireq textField "password"
     msys   <- runInputPost $ iopt textField "system"
-    let iden = case msys of
-		Nothing       -> GCM  regId
-                Just "WPhone" -> MPNS regId
+    iden   <- case msys of
+                Nothing       -> return $ GCM  regId
+                Just "WPhone" -> return $ MPNS regId
+                Just _        -> invalidArgs []
     device <- runDB $ getBy $ UniqueUser usr
     $(logInfo) ("\nIntent for a new user!:\n-User: "<> usr <>"\n-Password: "<> pass <>"\n-Identifier: "<> (pack $ show iden) <>"\n")
     case device of
         Nothing -> do
-                        dev  <-  runDB $ getBy $ UniqueIdent iden
-                        case dev of
-                            Just x  ->  do
-                                            runDB $ update (entityKey (x)) [DevicesUser =. usr , DevicesPassword =. pass ]
-                                            sendResponse $ RepJson emptyContent
-                            Nothing ->  do
-                                            runDB $ insert $ Devices usr pass iden
-                                            sendResponse $ RepJson emptyContent
+                       dev  <-  runDB $ getBy $ UniqueDevice iden
+                       case dev of
+                           Just x  ->  do
+                                           runDB $ update (entityKey (x)) [DevicesUser =. usr , DevicesPassword =. pass ]
+                                           sendResponse $ RepJson emptyContent
+                           Nothing ->  do
+                                           runDB $ insert $ Devices usr pass iden
+                                           sendResponse $ RepJson emptyContent
         Just a	-> case devicesPassword (entityVal (a)) == pass of
-                        True  -> do -- regId has changed.
+                       True  -> do -- regId has changed.
                                     runDB $ update (entityKey (a)) [DevicesIdentifier =. iden ]
                                     sendResponse $ RepJson emptyContent
-                        False -> invalidArgs []
+                       False -> invalidArgs []
 
 -- 'postFromWebR' receives messages from the website user. (POST messages to '/fromweb')
 postFromWebR :: Handler ()
@@ -142,34 +142,34 @@ postFromWebR = do
   cl <- runInputPost $ iopt textField "clear"
   case cl of
    Just _ -> do
-                    deleteSession "history"
-                    redirect RootR
+                 deleteSession "history"
+                 redirect RootR
    _      -> do
     msg  <- runInputPost $ ireq textField "message" 
     dest <- runInputPost $ ireq textField "destination"
     list <- case dest of
-                    "EveryOne"  ->  runDB $ selectList [] [Desc DevicesIdentifier]
-                    usr         ->  do
-                                        res <- runDB $ getBy $ UniqueUser usr
-                                        case res of
-                                            Just a  -> return [a]
-                                            Nothing -> return []
+                "EveryOne"  -> runDB $ selectList [] [Desc DevicesIdentifier]
+                usr         -> do
+                                   res <- runDB $ getBy $ UniqueUser usr
+                                   case res of
+                                       Just a  -> return [a]
+                                       Nothing -> return []
     Messages _ _ appConfig man <- getYesod
     $(logInfo) ("\tA new message: \""<>msg<>"\"\t")
     let regIdsList = map (\a -> devicesIdentifier(entityVal a)) list
     if regIdsList /= []
     then do 
-            let message = Message{
-                         gcmMessage  = def{registration_ids = Just (gcmDevices regIdsList) , data_object = Just (fromList [(pack "Message" .= msg)]) }
-                     ,   mpnsMessage = def {deviceURIs = mpnsDevices regIdsList , target = Toast , rest = Document (Prologue [] Nothing []) (xmlMessage msg) []}
+            let message = def {
+                         gcmNotif  = Just $ def {data_object = Just (fromList [(pack "Message" .= msg)]) }
+                     ,   mpnsNotif = Just $ def {target = Toast , rest = Document (Prologue [] Nothing []) (xmlMessage msg) []}
                      }
             result <- liftIO $ CE.catch -- I catch IO exceptions to avoid showing unsecure information.
-                        (send man appConfig message)
+                        (send man appConfig message regIdsList)
                         (\e -> do
-                                    let _ = (e :: CE.SomeException)
-                                    fail "Problem communicating with Push Servers")
+                                   let _ = (e :: CE.SomeException)
+                                   fail "Problem communicating with Push Servers")
             return ()
-            --handleResult gcmMessage gcmResult
+            handleResult message result
     else do
             return ()
     his <- lookupSession "history"
@@ -178,12 +178,6 @@ postFromWebR = do
         Nothing -> setSession "history" msg
     redirect RootR
     where
-        gcmDevices  = map (\(GCM  x) -> x) . filter (\x -> case x of 
-                                                         GCM _ -> True
-                                                         _     -> False)
-        mpnsDevices = map (\(MPNS x) -> x) . filter (\x -> case x of 
-                                                         MPNS _ -> True
-                                                         _      -> False)
         xmlMessage msg = Element (Name "Notification" (Just "WPNotification") (Just "wp")) (M.singleton "xmlns:wp" "WPNotification") [xml|
 <wp:Toast>
     <wp:Text1>New message: 
@@ -191,30 +185,29 @@ postFromWebR = do
     <wp:Param>?msg=#{msg}
 |]
 
--- Handle the result of the communication with the GCM Server.
-handleResult :: GCMmessage -> GCMresult -> Handler ()
+-- Handle the result of the communication with the Push Servers.
+handleResult :: PushNotification -> PushResult -> Handler ()
 handleResult msg res = do
-                        handleNewRegIDs $ newRegids res
-                        handleUnRegistered $ errorUnRegistered res
-                        handleToResend msg $ errorToReSend res
-                        handleRest $ errorRest res
-                        
--- Handle the regIds that have changed, I neew to actualize the DB.
-handleNewRegIDs :: [(RegId,RegId)] -> Handler ()
+                        handleNewRegIDs    $ newIds       res
+                        handleUnRegistered $ unRegistered res
+                        handleToResend msg $ toResend     res
+
+-- Handle the regIds that have changed, I need to actualize the DB.
+handleNewRegIDs :: [(Device,Device)] -> Handler ()
 handleNewRegIDs newRegIDs = do
                         $(logInfo) ("\tHandling NewRegIds: "<>pack (show newRegIDs)<>"\t")
                         let replaceOldRegId (old,new) = do
-                                            dev  <-  runDB $ getBy $ UniqueIdent (GCM old)
+                                            dev  <-  runDB $ getBy $ UniqueDevice old
                                             case dev of
-                                                Just x  ->  runDB $ update (entityKey (x)) [DevicesIdentifier =. (GCM new) ]
+                                                Just x  ->  runDB $ update (entityKey (x)) [DevicesIdentifier =. new ]
                                                 Nothing ->  return ()
                         foldr (>>) (return ()) $ map replaceOldRegId $ newRegIDs
 
 -- Handle the regIds that have been unregistered, I neew to remove them from the DB. (Unregistered error)
-handleUnRegistered :: [RegId] -> Handler ()
+handleUnRegistered :: [Device] -> Handler ()
 handleUnRegistered unRegIDs = do
                         $(logInfo) ("\tHandling UnRegistered: "<>pack (show unRegIDs)<>"\t")
-                        let removeUnRegUser regid = runDB $ deleteBy $ UniqueIdent (GCM regid)
+                        let removeUnRegUser regid = runDB $ deleteBy $ UniqueDevice regid
                         foldr (>>) (return ()) $ map removeUnRegUser $ unRegIDs
 
 -- I decide to unregister all regId with error different to Unregistered or Unavailable.
@@ -222,22 +215,22 @@ handleUnRegistered unRegIDs = do
 handleRest :: [(RegId,Text)] -> Handler ()
 handleRest rest = do
                     $(logInfo) ("\tHandling RestError: "<>pack (show rest)<>"\t")
-                    let removeUnRegUser (regid,_) = runDB $ deleteBy $ UniqueIdent (GCM regid)
+                    let removeUnRegUser (regid,_) = runDB $ deleteBy $ UniqueDevice (GCM regid)
                     foldr (>>) (return ()) $ map removeUnRegUser $ rest
 
 -- Handle the regIds that I need to resend because of an internal server error. (Unavailable error)
-handleToResend :: GCMmessage -> [RegId] -> Handler ()
+handleToResend :: PushNotification -> [Device] -> Handler ()
 handleToResend msg list = do
                     $(logInfo) ("\tHandling ToReSend: "<>pack (show list)<>"\t")
                     if list /= []
                     then do
-                        Messages _ _ (AppConfig gcmcnfg _) m <- getYesod
-                        gcmResult <- liftIO $ CE.catch -- I catch IO exceptions to avoid showing unsecure information.
-                                    (sendGCM m gcmcnfg msg{registration_ids = Just list})
+                        Messages _ _ appConfig m <- getYesod
+                        res <- liftIO $ CE.catch -- I catch IO exceptions to avoid showing unsecure information.
+                                    (send m appConfig msg list)
                                     (\e -> do
-                                                let _ = (e :: CE.SomeException)
-                                                fail "Problem communicating with GCM Server")
-                        handleResult msg{registration_ids = Just list} gcmResult
+                                              let _ = (e :: CE.SomeException)
+                                              fail "Problem communicating with GCM Server")
+                        handleResult msg res
                     else do
                         return ()
 
@@ -249,7 +242,7 @@ main = do
     man <- liftIO $ newManager def
     liftIO $ do
                 static@(Static settings) <- static "static"
-                warpDebug 3000 $ Messages pool static AppConfig{
-                                                     gcmAppConfig  = GCMAppConfig "key=" 5 -- Here you must complete with the correct Api Key provided by Google.
-                                                 ,   mpnsAppConfig = def
+                warp 3000 $ Messages pool static def{
+                                                     gcmAppConfig  = Just $ GCMAppConfig "key=" 5 -- Here you must complete with the correct Api Key provided by Google.
+                                                 ,   mpnsAppConfig = Just def
                                                  } man
