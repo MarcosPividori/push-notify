0f95b3b5be1b6ffda4dc1a1f4db71b6980620b1f
MarcosPividori <marcos.pividori@gmail.com>
2013-07-31 11:16:14 -0300

    actualize WP app

---
+++ push-notify/test/WindowsPhone-App/GSoC-Example/GSoC-Example/LocalizedStrings.cs
@@ -0,0 +1,11 @@
+﻿using GSoC_Example.Resources;
+
+namespace GSoC_Example
+{
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
