33dea1a3c57d0e52d39dea6bbe43ea07dedea8ac
Marcos Pividori <marcos.pividori@gmail.com>
2013-08-05 11:32:02 -0300

    changes in new abstraction.

--- push-notify/Network/PushNotify/YesodPushApp.hs
+++ push-notify/Network/PushNotify/YesodPushApp.hs
@@ -6,57 +6,48 @@ module YesodPushApp where
 import Yesod
 import Network.PushNotify.General
 import Control.Concurrent
+import Data.Text
 
-data YesodPushApp = YesodPushApp {
+data RegisterResult = Successful | ErrorReg Text
+
+data PushAppSub = PushAppSub {
                             newmessageCallback   :: Device -> Value -> IO ()
-                         ,  newdeviceCallback    :: Device -> Value -> IO Bool
+                         ,  newdeviceCallback    :: Device -> Value -> IO RegisterResult
                          }
 
-mkYesod "YesodPushApp" [parseRoutes|
-/register RegisterR POST
-/messages MessagesR POST
+mkYesodSubData "PushAppSub" [parseRoutes|
+/register SubRegisterR POST
+/messages SubMessagesR POST
 |]
 
--- Instances:
-
-instance Yesod YesodPushApp
-
-instance RenderMessage YesodPushApp FormMessage where
-    renderMessage _ _ = defaultFormMessage
-
 -- 'postRegister' allows a mobile device to register. (POST messages to '/register')
-postRegisterR :: Handler ()
-postRegisterR = do
-    regId  <- runInputPost $ ireq textField "regId"
-    msys   <- runInputPost $ iopt textField "system"
+postSubRegisterR :: (RenderMessage master FormMessage, Yesod master) => HandlerT PushAppSub (HandlerT master IO) ()
+postSubRegisterR = do
+    regId  <- lift $ runInputPost $ ireq textField "regId"
+    msys   <- lift $ runInputPost $ iopt textField "system"
     iden   <- case msys of
                 Nothing       -> return $ GCM  regId -- We take GCM as default.
                 Just "WPhone" -> return $ MPNS regId -- A WPhone device
                 Just "iOS"    -> return $ APNS regId -- A iOS device.
                 Just _        -> invalidArgs []
-    value  <- parseJsonBody_
-    YesodPushApp _ callback <- getYesod
+    value  <- lift $ parseJsonBody_
+    PushAppSub _ callback <- getYesod
     res <- liftIO $ callback iden value
-    if res 
-      then sendResponse $ RepJson emptyContent -- sucessful registration.
-      else invalidArgs []                      -- error in registration.
+    case res of
+      Successful -> sendResponse $ RepJson emptyContent -- sucessful registration.
+      ErrorReg t -> permissionDenied t                  -- error in registration.
 
 -- 'postMessages' allows a mobile device to send a message. (POST messages to '/messages')
-postMessagesR :: Handler ()
-postMessagesR = do
-    regId  <- runInputPost $ ireq textField "regId"
-    msys   <- runInputPost $ iopt textField "system"
+postSubMessagesR :: (RenderMessage master FormMessage, Yesod master) => HandlerT PushAppSub (HandlerT master IO) ()
+postSubMessagesR = do
+    regId  <- lift $ runInputPost $ ireq textField "regId"
+    msys   <- lift $ runInputPost $ iopt textField "system"
     iden   <- case msys of
                 Nothing       -> return $ GCM  regId -- We take GCM as default.
                 Just "WPhone" -> return $ MPNS regId -- A WPhone device.
                 Just "iOS"    -> return $ APNS regId -- A iOS device.
                 Just _        -> invalidArgs []
-    value  <- parseJsonBody_
-    YesodPushApp callback _ <- getYesod
+    value  <- lift $ parseJsonBody_
+    PushAppSub callback _ <- getYesod
     liftIO $ forkIO $ callback iden value
     sendResponse $ RepJson emptyContent
-
-startYesodApp :: (Device -> Value -> IO ())
-              -> (Device -> Value -> IO Bool)
-              -> IO ()
-startYesodApp cb1 cb2 = warp 3000 $ YesodPushApp cb1 cb2
