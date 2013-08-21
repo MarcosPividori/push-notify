4caea3f0a66d2b535e3c266816a2523cb502690f
Marcos Pividori <marcos.pividori@gmail.com>
2013-08-20 23:33:43 -0300

    changes in package distribution.

---
+++ push-notify-general/Network/PushNotify/YesodPushAppRoutes.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE OverloadedStrings, TypeFamilies, TemplateHaskell, QuasiQuotes #-}
+
+module Network.PushNotify.YesodPushAppRoutes where
+
+import Yesod
+import Network.PushNotify.General
+import Control.Concurrent
+import Data.Text
+
+data RegisterResult = SuccessfulReg | ErrorReg Text
+
+data PushAppSub = PushAppSub {
+                            newmessageCallback   :: Device -> Value -> IO ()
+                         ,  newdeviceCallback    :: Device -> Value -> IO RegisterResult
+                         }
+
+mkYesodSubData "PushAppSub" [parseRoutes|
+/register SubRegisterR POST
+/messages SubMessagesR POST
+|]
