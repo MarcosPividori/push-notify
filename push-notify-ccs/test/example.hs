5c1a224f0fd8689bc1836b164119da24301438fa
Marcos Pividori <marcos.pividori@gmail.com>
2013-08-25 19:39:35 -0300

    details

--- push-notify-ccs/test/example.hs
+++ push-notify-ccs/test/example.hs
@@ -2,13 +2,22 @@
 {-# LANGUAGE OverloadedStrings, TypeFamilies, TemplateHaskell,
              QuasiQuotes, MultiParamTypeClasses, GeneralizedNewtypeDeriving, FlexibleContexts, GADTs #-}
 
-import Send
-import Network.PushNotify.Gcm
+import Network.PushNotify.Ccs
 import Data.Default
+import qualified Data.HashMap.Strict  as HM
+import Data.Aeson
+import Data.Text
 
 main :: IO ()
 main = do
          man <- startCCS (GCMAppConfig "ApiKey" "senderId" 5)
-                         (\_ _ -> return ())
-         sendCCS man def
+                         (\r dat -> do
+                                      putStrLn "A new Message from device!: " 
+                                      putStrLn $ "\t From: " ++ show r
+                                      putStrLn $ "\t Data: " ++ show dat
+                                      return ())
+         sendCCS man def{
+                       registration_ids = ["Here the regId of a device."]
+                     , data_object = Just (HM.fromList [(pack "Message" .= pack "hola")]) -- Example of JSON data.
+                     }
          closeCCS man
