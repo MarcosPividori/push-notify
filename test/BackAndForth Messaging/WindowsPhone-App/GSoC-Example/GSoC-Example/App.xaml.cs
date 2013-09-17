415a4b5027287e34608263264973ab7ed9b6ab79
MarcosPividori <marcos.pividori@gmail.com>
2013-09-17 17:17:05 -0300

    actualizing WPhone app.

---
+++ test/BackAndForth Messaging/WindowsPhone-App/GSoC-Example/GSoC-Example/App.xaml.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Diagnostics;
+using System.Resources;
+using System.Windows;
+using System.Windows.Markup;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using GSoC_Example.Resources;
+
+namespace GSoC_Example
+{
+    public partial class App : Application
+    {
+        // Provides easy access to the root frame of the Phone Application.
+        public static PhoneApplicationFrame RootFrame { get; private set; }
+
+        // Constructor for the Application object.
+        public App()
+        {
+            // Global handler for uncaught exceptions.
+            UnhandledException += Application_UnhandledException;
+
+            // Standard Silverlight initialization
+            InitializeComponent();
+
+            // Phone-specific initialization
+            InitializePhoneApplication();
+
+            // Initialization of the language
+            InitializeLanguage();
+
+            if (Debugger.IsAttached)
+            {
+                Application.Current.Host.Settings.EnableFrameRateCounter = true;
+                PhoneApplicationService.Current.UserIdleDetectionMode = IdleDetectionMode.Disabled;
+            }
+            RootFrame.Navigating += new NavigatingCancelEventHandler(RootFrame_Navigating);
+        }
+
+        void RootFrame_Navigating(object sender, NavigatingCancelEventArgs e)
+        {
+            if (e.Uri.ToString().Contains("/MainPage.xaml") != true) 
+                return;
+            if (! System.IO.IsolatedStorage.IsolatedStorageSettings.ApplicationSettings.Contains("user") )
+            {
+                e.Cancel = true;
+                RootFrame.Dispatcher.BeginInvoke(delegate
+                {
+                    RootFrame.Navigate(new Uri("/Register.xaml", UriKind.Relative));
+                });
+            }
+        }
+        
+        private void Application_Launching(object sender, LaunchingEventArgs e)
+        {
+        }
+
+        private void Application_Activated(object sender, ActivatedEventArgs e)
+        {
+        }
+
+        private void Application_Deactivated(object sender, DeactivatedEventArgs e)
+        {
+        }
+
+        private void Application_Closing(object sender, ClosingEventArgs e)
+        {
+        }
+
+        private void RootFrame_NavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            if (Debugger.IsAttached)
+            {
+                Debugger.Break();
+            }
+        }
+
+        private void Application_UnhandledException(object sender, ApplicationUnhandledExceptionEventArgs e)
+        {
+            if (Debugger.IsAttached)
+            {
+                Debugger.Break();
+            }
+        }
+
+        #region Inicialización de la aplicación telefónica
+
+        // Prevent double initialisation
+        private bool phoneApplicationInitialized = false;
+
+        // Do not add any additional code to this method
+        private void InitializePhoneApplication()
+        {
+            if (phoneApplicationInitialized)
+                return;
+
+            RootFrame = new PhoneApplicationFrame();
+            RootFrame.Navigated += CompleteInitializePhoneApplication;
+
+            RootFrame.NavigationFailed += RootFrame_NavigationFailed;
+
+            RootFrame.Navigated += CheckForResetNavigation;
+
+            phoneApplicationInitialized = true;
+        }
+
+        // Do not add any additional code to this method
+        private void CompleteInitializePhoneApplication(object sender, NavigationEventArgs e)
+        {
+            if (RootVisual != RootFrame)
+                RootVisual = RootFrame;
+
+            RootFrame.Navigated -= CompleteInitializePhoneApplication;
+        }
+
+        private void CheckForResetNavigation(object sender, NavigationEventArgs e)
+        {
+           if (e.NavigationMode == NavigationMode.Reset)
+                RootFrame.Navigated += ClearBackStackAfterReset;
+        }
+
+        private void ClearBackStackAfterReset(object sender, NavigationEventArgs e)
+        {
+            RootFrame.Navigated -= ClearBackStackAfterReset;
+
+            if (e.NavigationMode != NavigationMode.New && e.NavigationMode != NavigationMode.Refresh)
+                return;
+
+            while (RootFrame.RemoveBackEntry() != null)
+            {
+                ;
+            }
+        }
+
+        #endregion
+
+        private void InitializeLanguage()
+        {
+            try
+            {
+                RootFrame.Language = XmlLanguage.GetLanguage(AppResources.ResourceLanguage);
+
+                FlowDirection flow = (FlowDirection)Enum.Parse(typeof(FlowDirection), AppResources.ResourceFlowDirection);
+                RootFrame.FlowDirection = flow;
+            }
+            catch
+            {
+                if (Debugger.IsAttached)
+                {
+                    Debugger.Break();
+                }
+
+                throw;
+            }
+        }
+    }
+}
---
+++ test/BackAndForth Messaging/WindowsPhone-App/GSoC-Example/GSoC-Example/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+[assembly: AssemblyTitle("GSoC_Example")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GSoC_Example")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8d350009-15ad-4d4e-84da-a48f02b1cbf0")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("es-ES")]
