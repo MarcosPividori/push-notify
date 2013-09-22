02f03130b836c84868d1370ebcda8c3447619c7d
Marcos Pividori <marcos.pividori@gmail.com>
2013-09-21 21:13:08 -0300

    details

--- push-notify-general/test/Yesod-App/Extra.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE OverloadedStrings, TypeFamilies, TemplateHaskell,
-               QuasiQuotes, MultiParamTypeClasses, GeneralizedNewtypeDeriving, FlexibleContexts, GADTs #-}
-               
-module Extra where
-
-import Network.PushNotify.General
-import Database.Persist.TH
-
-derivePersistField "Device"
